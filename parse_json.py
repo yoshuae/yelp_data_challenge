abe2064f86d62fcaa53c7418639cafae929c3684
yoshuae <yoshuae@users.noreply.github.com>
2014-09-16 14:42:19 -0400

    Update parse_json.py

--- parse_json.py
+++ parse_json.py
@@ -1,3 +1,8 @@
+# this function inputs a json file where each line is a json record and outputs a python data structure which is a dict of dicts
+# the first level will index by numeric indices and the second level ( each json's info ) by the json fields
+
+# examples jdata[200]['fans'] or jdata[300].keys()
+
 def parse_json(file):
 
 	import json
