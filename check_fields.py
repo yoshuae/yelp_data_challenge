12cdaffca9c991fa1e7ec6660db80975fe877d39
yoshuae <jae83@cornell.edu>
2014-10-17 23:29:28 -0400

    writes out a csv with parsed json

--- check_fields.py
+++ check_fields.py
@@ -2,6 +2,7 @@
 execfile('../parse_json.py')
 execfile('../parse_user.py')
 execfile('../network_troll.py')
+
 # data is loaded in pwd
 users=parse_user('user.json')
 tips=parse_json('tip.json')
@@ -28,30 +29,30 @@ pop_avg_friends=sum(num_friends)*1.0/len(num_friends)
 num_fans=[users[x]['fans'] for x in range(len(users))]
 # population average number of fans ~1.67
 pop_avg_fans=sum(num_fans)*1.0/len(num_fans)
-
+temp_users=parse_user('user.json')
 # reach of each user
-reach=find_reach(users)
-
+reach=find_reach(temp_users)  # return list with counts, not dict with key user_id
 # build pandas df of user info pass to csv for analysis with R
 import pandas as pd
 users=parse_json('user.json') # reinitialize so we can access by number key rather than user_id
 user_name=[users[x]['name'] for x in range(len(users))]
-df=pd.DataFrame(data=user_name)
+df=pd.DataFrame(data=user_name,columns=['name'])
+
 df['avg_stars']=[users[x]['average_stars'] for x in range(len(users)) ]
 df['review_count']=[users[x]['review_count'] for x in range(len(users)) ]
-df['user_reach']=[reach[reach.keys()[x]] for x in range(len(reach))]  #number of friends + their friends 
 df['yelping_since']=[users[x]['yelping_since'] for x in range(len(users)) ]
 df['num_friends']=[len(users[x]['friends']) for x in range(len(users))] # just number of immediate friends
 df['fans']=[users[x]['review_count'] for x in range(len(users))]
 df['funny_votes']=[users[x]['votes']['funny'] for x in range(len(users))]
 df['useful_votes']=[users[x]['votes']['useful'] for x in range(len(users))]
 df['cool_votes']=[users[x]['votes']['cool'] for x in range(len(users))]
+df['user_id']=[users[x]['user_id'] for x in range(len(users))]
+df['user_reach']=[reach[x] for x in df['user_id']]                       ]  #number of friends + their friends 
 
+df=df.drop('user_id',1) # drop extra column
 
-
-
-
-
+# write to csv and do analysis in R :) 
+df.to_csv('parsed_to_df.csv',index=False,encoding='utf8')
 
 
 
